eaf863baf60dacaf7cca167a9955b596517840d0
Tobias Mullen <tobiasjmullen@gmail.com>
2019-05-04 17:14:16 -0600

    TimeStampLog class created

---
+++ src/com/mullen/viewlogtest/TimeStampLog.java
@@ -0,0 +1,12 @@
+package com.mullen.viewlogtest;
+
+import java.util.ArrayList;
+
+public class TimeStampLog {
+
+	private ArrayList<ViewLog> sectionsWatched;
+	
+	public TimeStampLog() {
+		sectionsWatched = new ArrayList<ViewLog>(); 
+	}
+}
