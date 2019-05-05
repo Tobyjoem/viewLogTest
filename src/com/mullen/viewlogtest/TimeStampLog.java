d2fb5aabee36c9c2387fcff352dc7a4f2fe0a807
Tobias Mullen <tobiasjmullen@gmail.com>
2019-05-04 18:06:56 -0600

    TimeStampLog.toString() overridden to match unit tests. A method to get the total time of of ViewLogs in the TimeStampLog was added.

--- src/com/mullen/viewlogtest/TimeStampLog.java
+++ src/com/mullen/viewlogtest/TimeStampLog.java
@@ -14,7 +14,29 @@ public class TimeStampLog {
 		
 	}
 	
+	/**
+	 * This function calculates the total time of unique video watched.
+	 * 
+	 * @return The total time in milliseconds
+	 */
+	public int getTotalTimeWatched() {
+		int totalTimeWatched = 0;
+		for (ViewLog view : sectionsWatched) {
+			totalTimeWatched += (view.getEndTime() - view.getStartTime());
+		}
+		return totalTimeWatched;
+	}
+	
+	@Override
 	public String toString() {
-		return "";
+		String results = "";
+		
+		for (ViewLog view : sectionsWatched) {
+			results = results + view.getStartTime() + "-" + view.getEndTime() + ",";
+		}
+		
+		results = results + "\n" + getTotalTimeWatched() + " milliseconds watched.";
+		
+		return results;
 	}
 }
