8968b172b91624f9894056a14e0b40690fb5d1c8
Tobias Mullen <tobiasjmullen@gmail.com>
2019-05-04 18:30:18 -0600

    First unit test passed!

--- src/com/mullen/viewlogtest/TimeStampLog.java
+++ src/com/mullen/viewlogtest/TimeStampLog.java
@@ -10,8 +10,40 @@ public class TimeStampLog {
 		sectionsWatched = new ArrayList<ViewLog>(); 
 	}
 	
+	/**
+	 * This function adds individual ViewLogs to the sectionsWatched ArrayList. If a new ViewLog overlaps
+	 * or is one millisecond away from another ViewLog in the ArrayList, this function combines ViewLogs
+	 * into one unique view.
+	 * 
+	 * @param view The ViewLog detailing the start and end times of the viewer's watching session
+	 */
 	public void addViewLog(ViewLog view) {
+		//If the ArrayList is empty, then the new section is unique.
+		if (sectionsWatched.isEmpty()) {
+			sectionsWatched.add(view);
+			return;
+		}
 		
+		//If the new ViewLog ends before any other ViewLog starts, it is placed in the front of the ArrayList.
+		if (view.getEndTime() <= sectionsWatched.get(0).getStartTime()) {
+			sectionsWatched.add(0, view);
+			return;
+		}
+		
+		//If the new ViewLog starts after all other ViewLogs have ended, it is placed at the end of the ArrayList.
+		if (view.getStartTime() >= sectionsWatched.get(sectionsWatched.size()-1).getEndTime()) {
+			sectionsWatched.add(view);
+			return;
+		}
+		
+		//If the new ViewLog falls completely between two existing ViewLogs, the new ViewLog is placed between them.
+		for (int i = 0; i < sectionsWatched.size(); i++) {
+			if (view.getStartTime() > sectionsWatched.get(i).getEndTime() &&
+				view.getEndTime() < sectionsWatched.get(i + 1).getStartTime()) {
+				sectionsWatched.add(i+1, view);
+				return;
+			}
+		}
 	}
 	
 	/**
